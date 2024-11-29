f47672936626c3a7cbf03247e020dc69040ddd61
jakub627 <dybich89@gmail.com>
2024-11-29 14:25:28 +0100

    added acceleration mechanics

--- bots.py
+++ bots.py
@@ -13,9 +13,10 @@ class Bot(Player):
         self._player: Player = player
         self.type: str = "default"
 
+        # Choosing where to spawn
         while True:
-            self.x: int = randint(0, WINDOW_WIDTH)
-            self.y: int = randint(0, WINDOW_HEIGHT)
+            self.x: int = randint(-WINDOW_WIDTH, WINDOW_WIDTH)
+            self.y: int = randint(-WINDOW_HEIGHT, WINDOW_HEIGHT)
             self.spawn_distance_from_player: float = np.sqrt(
                 (self.x + BOT_DEFAULT_WIDTH / 2 - self._player.x - PLAYER_WIDTH / 2)
                 ** 2
@@ -41,18 +42,21 @@ class Bot(Player):
             ),
         )
 
-    def _get_vector_to_player_parameters(self) -> np.ndarray:
+    def _get_vector_to_player_parameters(self, bot_width, bot_height) -> np.ndarray:
         vector_to_player: np.ndarray = np.array(
             [
-                self.x + BOT_DEFAULT_WIDTH / 2 - self._player.x - PLAYER_WIDTH / 2,
-                self.y + BOT_DEFAULT_HEIGHT / 2 - self._player.y - PLAYER_HEIGHT / 2,
+                self.x + bot_width / 2 - (self._player.x + PLAYER_WIDTH / 2),
+                self.y + bot_height / 2 - (self._player.y + PLAYER_HEIGHT / 2),
             ]
         )
         return vector_to_player
 
     def move_to_player(self):
-        vector: np.ndarray = self._get_vector_to_player_parameters()
+        vector: np.ndarray = self._get_vector_to_player_parameters(
+            BOT_DEFAULT_WIDTH, BOT_DEFAULT_HEIGHT
+        )
 
+        # Random movement, but most likely towards player
         angle = np.random.normal(0, 80)
         angle_rad = mth.radians(angle)
 
@@ -64,8 +68,11 @@ class Bot(Player):
                 dx * sin_theta + dy * cos_theta,
             ]
         )
+
+        # Pseudo collisions
         if np.linalg.norm(rotated_vector) < PLAYER_RADIUS + BOT_DEFAULT_RADIUS:
             return
+
         rotated_vector_normalized = rotated_vector / np.linalg.norm(rotated_vector)
         self.x -= rotated_vector_normalized[0] * BOT_DEFAULT_SPEED
         self.y -= rotated_vector_normalized[1] * BOT_DEFAULT_SPEED
@@ -74,7 +81,10 @@ class Bot(Player):
 class BotSprinter(Bot):
     def __init__(self, player: Player) -> None:
         super().__init__(player)
-        self.type: str = "default"
+        self.type: str = "sprinter"
+        self.start_time = None
+        self.current_speed = 0
+        self.moving = True
 
     def draw_bot(self, screen: pygame.Surface) -> None:
         pygame.draw.rect(
@@ -89,11 +99,37 @@ class BotSprinter(Bot):
         )
 
     def move_to_player(self):
-        vector: np.ndarray = self._get_vector_to_player_parameters()
+        if self.start_time is None or not self.moving:
+            self.start_time = pygame.time.get_ticks()
+            self.moving = True
+        elapsed_time = (pygame.time.get_ticks() - self.start_time) / 1000
+
+        # Acceleration
+        if elapsed_time < BOT_SPRINTER_TIME_TO_MAX_SPEED:
+            self.current_speed = BOT_SPRINTER_SPEED * (
+                elapsed_time / BOT_SPRINTER_TIME_TO_MAX_SPEED
+            )
+        else:
+            self.current_speed = BOT_SPRINTER_SPEED
+
+        vector: np.ndarray = self._get_vector_to_player_parameters(
+            BOT_SPRINTER_WIDTH, BOT_DEFAULT_HEIGHT
+        )
         vector_normalized = vector / np.linalg.norm(vector)
 
-        if np.linalg.norm(vector) < PLAYER_RADIUS + BOT_DEFAULT_RADIUS:
+        new_x = self.x - vector_normalized[0] * self.current_speed
+        new_y = self.y - vector_normalized[1] * self.current_speed
+
+        current_distance_to_player = np.sqrt(
+            (new_x + BOT_SPRINTER_WIDTH / 2 - self._player.x - PLAYER_WIDTH / 2) ** 2
+            + (new_y + BOT_SPRINTER_HEIGHT / 2 - self._player.y - PLAYER_HEIGHT / 2)
+            ** 2
+        )
+
+        # Pseudo collisions
+        if current_distance_to_player < PLAYER_RADIUS + BOT_SPRINTER_RADIUS:
+            self.moving = False
             return
 
-        self.x -= vector_normalized[0] * BOT_DEFAULT_SPEED
-        self.y -= vector_normalized[1] * BOT_DEFAULT_SPEED
+        self.x = new_x
+        self.y = new_y
